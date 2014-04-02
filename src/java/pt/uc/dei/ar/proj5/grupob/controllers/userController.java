f7e02e6d31ffa26cea155ac608e3624d026ed348
Ana Sofia Mendes <ana_smendes@gmail.com>
2014-04-02 21:45:41 +0100

    new files

---
+++ src/java/pt/uc/dei/ar/proj5/grupob/controllers/userController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.uc.dei.ar.proj5.grupob.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author sofia
+ */
+@ManagedBean
+@RequestScoped
+public class userController {
+
+    /**
+     * Creates a new instance of userController
+     */
+    public userController() {
+    }
+    
+}

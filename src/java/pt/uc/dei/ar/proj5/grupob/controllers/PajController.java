d193409df25f308485b81f1bc4264230d14c46d1
Ana Sofia Mendes <ana_smendes@gmail.com>
2014-04-03 15:07:37 +0100

    paj

--- src/java/pt/uc/dei/ar/proj5/grupob/controllers/PajController.java
+++ src/java/pt/uc/dei/ar/proj5/grupob/controllers/PajController.java
@@ -3,21 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pt.uc.dei.ar.proj5.grupob.controllers;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import pt.uc.dei.ar.proj5.grupob.entities.Paj;
 
 /**
  *
  * @author sofia
  */
-
 @Named
 @RequestScoped
 public class PajController {
+
+    private Paj paj;
+
+    public PajController() {
+    }
+
+    public Paj getPaj() {
+        return paj;
+    }
+
+    public void setPaj(Paj paj) {
+        this.paj = paj;
+    }
     
     
-    
+
+    @PostConstruct
+    public void initPajController() {
+        this.paj = new Paj();
+    }
+
 }

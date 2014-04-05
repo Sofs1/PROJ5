38f6d85588b06d49971527719e2ce7927e10e27c
Ana Sofia Mendes <ana_smendes@gmail.com>
2014-04-05 15:14:34 +0100

    asnj

--- src/java/pt/uc/dei/ar/proj5/grupob/controllers/UserController.java
+++ src/java/pt/uc/dei/ar/proj5/grupob/controllers/UserController.java
@@ -32,7 +32,7 @@ import pt.uc.dei.ar.proj5.grupob.util.PasswordException;
 @Named
 @RequestScoped
 public class UserController {
-    
+
     @Inject
     private PajFacade pajFacade;
     @Inject
@@ -52,101 +52,102 @@ public class UserController {
      */
     public UserController() {
     }
-    
+
     @PostConstruct
     public void initUser() {
         this.student = new Student();
         this.user = new User();
     }
-    
+
     public Paj getSelectedPaj() {
         return selectedPaj;
     }
-    
+
     public void setSelectedPaj(Paj selectedPaj) {
         this.selectedPaj = selectedPaj;
     }
-    
+
     public UserEJB getUserEJB() {
         return userEJB;
     }
-    
+
     public void setUserEJB(UserEJB userEJB) {
         this.userEJB = userEJB;
     }
-    
+
     public Student getStudent() {
         return student;
     }
-    
+
     public void setStudent(Student student) {
         this.student = student;
     }
-    
+
     public void setErro(String erro) {
         this.erro = erro;
     }
-    
+
     public String getErro() {
         return erro;
     }
-    
+
     public String getPassConf() {
         return passConf;
     }
-    
+
     public void setPassConf(String passConf) {
         this.passConf = passConf;
     }
-    
+
     public User getUser() {
         return user;
     }
-    
+
     public void setUser(User user) {
         this.user = user;
     }
-    
+
     public PajFacade getPajFacade() {
         return pajFacade;
     }
-    
+
     public void setPajFacade(PajFacade pajFacade) {
         this.pajFacade = pajFacade;
     }
-    
+
     public StudentFacade getStudentFacade() {
         return studentFacade;
     }
-    
+
     public void setStudentFacade(StudentFacade studentFacade) {
         this.studentFacade = studentFacade;
     }
-    
+
     public AdministratorFacade getAdminFacade() {
         return adminFacade;
     }
-    
+
     public void setAdminFacade(AdministratorFacade adminFacade) {
         this.adminFacade = adminFacade;
     }
-    
+
     public String createStudent() {
-        
+
         try {
             studentFacade.createStudent(student, passConf);
             student.setPaj(selectedPaj);
             selectedPaj.getStudents().add(student);
             studentFacade.edit(student);
             pajFacade.edit(selectedPaj);
-            return "index";
+            userEJB.setUser(student);
+            return "templateStudent";
         } catch (PasswordException | DuplicateEmailException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
             erro = ex.getMessage();
             return "signup";
         }
     }
-    
+
     public String searchLogged() {
         try {
             userEJB.setUser(studentFacade.searchStudent(user.getEmail(), user.getPass()));
@@ -157,7 +158,7 @@ public class UserController {
             return "index";
         }
     }
-    
+
     public String searchAdmin() {
         try {
             userEJB.setUser(adminFacade.searchAdmin(user.getEmail(), user.getPass()));
@@ -168,11 +169,11 @@ public class UserController {
             return "adminLogin";
         }
     }
-    
+
     public List<Paj> listAllPajs() {
         return pajFacade.findAll();
     }
-    
+
     public String editUser() {
         try {
             studentFacade.editStudentFacade(student, passConf, userEJB.getUser().getEmail());
@@ -182,32 +183,32 @@ public class UserController {
             erro = ex.getMessage();
             return null;
         }
-        
+
     }
-    
+
     public void removeUser() {
         studentFacade.deleteStudent((Student) userEJB.getUser(), userEJB.getPajSelected());
     }
-    
+
     public String logoutAdm() {
         userEJB.setUser(null);
         userEJB.setPajSelected(null);
         invalidateSession();
         return "index";
     }
-    
+
     public String logoutStud() {
         userEJB.setUser(null);
         invalidateSession();
         return "index";
     }
-    
+
     private void invalidateSession() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
         session.invalidate();
     }
-    
+
     public String editStudent() {
         try {
             studentFacade.editStudentFacade(student, passConf, userEJB.getUser().getEmail());
@@ -217,7 +218,7 @@ public class UserController {
             erro = ex.getMessage();
             return null;
         }
-        
+
     }
-    
+
 }

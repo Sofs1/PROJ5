41d9fcd9b49a15a3eedbe430f60c18f8446c4c7d
ziguri <orlandoluisneves@gmail.com>
2014-04-05 18:50:31 +0100

    comitt do singup

---
+++ src/java/pt/uc/dei/ar/proj5/grupob/controllers/RegistrationController.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.dei.ar.proj5.grupob.controllers;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import pt.uc.dei.ar.proj5.grupob.entities.Paj;
+import pt.uc.dei.ar.proj5.grupob.entities.Student;
+import pt.uc.dei.ar.proj5.grupob.facades.PajFacade;
+import pt.uc.dei.ar.proj5.grupob.facades.StudentFacade;
+import pt.uc.dei.ar.proj5.grupob.util.DuplicateEmailException;
+import pt.uc.dei.ar.proj5.grupob.util.PasswordException;
+
+/**
+ *
+ * @author Zueb LDA
+ */
+@Named("registrationController")
+@ConversationScoped
+public class RegistrationController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private Conversation conversation;
+    @Inject
+    private PajFacade pajFacade;
+    @Inject
+    private StudentFacade studentFacade;
+    private Student student;
+    private String passConf;
+    private String erro;
+    private Paj selectedPaj;
+
+    public RegistrationController() {
+    }
+
+    @PostConstruct
+    public void initUser() {
+        this.student = new Student();
+
+    }
+
+    public PajFacade getPajFacade() {
+        return pajFacade;
+    }
+
+    public void setPajFacade(PajFacade pajFacade) {
+        this.pajFacade = pajFacade;
+    }
+
+    public StudentFacade getStudentFacade() {
+        return studentFacade;
+    }
+
+    public void setStudentFacade(StudentFacade studentFacade) {
+        this.studentFacade = studentFacade;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getPassConf() {
+        return passConf;
+    }
+
+    public void setPassConf(String passConf) {
+        this.passConf = passConf;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public Paj getSelectedPaj() {
+        return selectedPaj;
+    }
+
+    public void setSelectedPaj(Paj selectedPaj) {
+        this.selectedPaj = selectedPaj;
+    }
+
+    public List<Paj> listAllPajs() {
+        return pajFacade.findAll();
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
+
+    public String signUp() {
+        conversation.begin();
+        return "signup";
+
+    }
+
+    public String createStudent() {
+
+        try {
+            studentFacade.createStudent(student, passConf);
+            student.setPaj(selectedPaj);
+            selectedPaj.getStudents().add(student);
+            studentFacade.edit(student);
+            pajFacade.edit(selectedPaj);
+            conversation.end();
+            return "index";
+
+        } catch (PasswordException | DuplicateEmailException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+            erro = ex.getMessage();
+            return "signup";
+        }
+    }
+
+    public String cancelView() {
+
+        conversation.end();
+        return "index";
+    }
+}
